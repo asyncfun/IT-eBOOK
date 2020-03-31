1c2ea508b64d00b733662c1ca16bf432c3f76e64
maguowei <imaguowei@gmail.com>
2020-03-31 16:05:27 +0800

    support page param

--- spider.py
+++ spider.py
@@ -1,4 +1,4 @@
-from requests_html import HTMLSession
+from requests_html import HTMLSession, user_agent
 
 
 SITE_URL = 'https://itpanda.net'
@@ -13,8 +13,10 @@ class Spider:
         nav = r.html.find('.nav', first=True)
         items = nav.find('.nav-item a')
         for item in items:
+            title, num = item.text.split(' ')
             cate = {
-                'title': item.text,
+                'title': title,
+                'num': num.lstrip('(').rstrip(')'),
                 'link': f"{SITE_URL}{item.attrs['href']}"
             }
             data.append(cate)
@@ -25,23 +27,34 @@ class Spider:
         for c in category:
             title = c['title']
             link = c['link']
-            r = session.get(link)
-            books_list = r.html.find('.list-unstyled', first=True)
-            books = books_list.find('.media .mt-0 a')
-            for book in books:
-                bool_title, book_link = book.text, f"{SITE_URL}{book.attrs['href']}"
-                # print(title, bool_title, book_link)
-                r = session.get(book_link)
-                downloads = r.html.find('.text-danger.mr-2')
-                download = downloads[1] if len(downloads) > 1 else downloads[0]
-                download_url = f"{SITE_URL}{download.attrs['href']}"
-                r = session.get(download_url)
+            num = int(c['num'])
 
-                pans = r.html.find('.text-danger')
-                pan = pans[1] if len(pans) > 1 else pans[0]
-                pan_url = pan.attrs['href']
-                pan_key = r.html.search('提取码:{}</p>')[0]
-                print(pan_url, pan_key)
+            book_num = 0
+            page = 1
+            while book_num < num:
+                r = session.get(f'{link}?sort_by=created_at&page={page}')
+                books_list = r.html.find('.list-unstyled', first=True)
+                books = books_list.find('.media .mt-0 a')
+
+                for book in books:
+                    book_num += 1
+                    bool_title, book_link = book.text, f"{SITE_URL}{book.attrs['href']}"
+                    pan_url, pan_key = self.get_book(book_link)
+                    print(title, num, bool_title, pan_url, pan_key)
+                page += 1
+
+    def get_book(self, book_link):
+        r = session.get(book_link)
+        downloads = r.html.find('.text-danger.mr-2')
+        download = downloads[1] if len(downloads) > 1 else downloads[0]
+        download_url = f"{SITE_URL}{download.attrs['href']}"
+        r = session.get(download_url)
+
+        pans = r.html.find('.text-danger')
+        pan = pans[1] if len(pans) > 1 else pans[0]
+        pan_url = pan.attrs['href']
+        pan_key = r.html.search('提取码:{}</p>')[0]
+        return pan_url, pan_key
 
 
 if __name__ == '__main__':
