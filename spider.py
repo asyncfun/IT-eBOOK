2b35abf7ce2e651010651bccecf9802851a38c99
maguowei <imaguowei@gmail.com>
2020-03-31 14:50:49 +0800

    get baidupan url and key

--- spider.py
+++ spider.py
@@ -32,8 +32,16 @@ class Spider:
                 bool_title, book_link = book.text, f"{SITE_URL}{book.attrs['href']}"
                 # print(title, bool_title, book_link)
                 r = session.get(book_link)
-                pan = r.html.find('.text-danger')
-                print(pan)
+                downloads = r.html.find('.text-danger.mr-2')
+                download = downloads[1] if len(downloads) > 1 else downloads[0]
+                download_url = f"{SITE_URL}{download.attrs['href']}"
+                r = session.get(download_url)
+
+                pans = r.html.find('.text-danger')
+                pan = pans[1] if len(pans) > 1 else pans[0]
+                pan_url = pan.attrs['href']
+                pan_key = r.html.search('提取码:{}</p>')[0]
+                print(pan_url, pan_key)
 
 
 if __name__ == '__main__':
