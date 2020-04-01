d220b78b2962c69a5b9bdffb5a303cf75936a300
maguowei <imaguowei@gmail.com>
2020-04-01 11:13:39 +0800

    add books.csv, use proxy

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # itpanda-crawler
+
+```bash
+# install deps
+$ pipenv install
+
+# set proxy
+$ export PROXIES_SERVER='xxx.xxx.xxx.xxx:8080'
+
+$ python spider.py
+```
--- spider.py
+++ spider.py
@@ -1,15 +1,21 @@
+import os
 from requests_html import HTMLSession
+from requests.adapters import HTTPAdapter
 
 
 SITE_URL = 'https://itpanda.net'
 
 session = HTMLSession()
+session.mount(SITE_URL, HTTPAdapter(max_retries=5))
+
+PROXIES_SERVER = os.environ.get('PROXIES_SERVER')
+PROXIES = {'http': PROXIES_SERVER, 'https': PROXIES_SERVER}
 
 
 class Spider:
     def get_category(self):
         data = []
-        r = session.get(SITE_URL)
+        r = session.get(SITE_URL, proxies=PROXIES)
         nav = r.html.find('.nav', first=True)
         items = nav.find('.nav-item a')
         for item in items:
@@ -32,23 +38,26 @@ class Spider:
             book_num = 0
             page = 1
             while book_num < num:
-                r = session.get(f'{link}?sort_by=created_at&page={page}')
+                r = session.get(f'{link}?sort_by=created_at&page={page}', proxies=PROXIES)
                 books_list = r.html.find('.list-unstyled', first=True)
                 books = books_list.find('.media .mt-0 a')
 
                 for book in books:
                     book_num += 1
                     bool_title, book_link = book.text, f"{SITE_URL}{book.attrs['href']}"
-                    pan_url, pan_key = self.get_book(book_link)
-                    print(f'{title}, {num}, {bool_title}, {pan_url}, {pan_key}')
+                    try:
+                        pan_url, pan_key = self.get_book(book_link)
+                        print(f'{title}, {bool_title}, {pan_url}, {pan_key}')
+                    except Exception as e:
+                        print(e, bool_title, book_link)
                 page += 1
 
     def get_book(self, book_link):
-        r = session.get(book_link)
+        r = session.get(book_link, proxies=PROXIES)
         downloads = r.html.find('.text-danger.mr-2')
         download = downloads[1] if len(downloads) > 1 else downloads[0]
         download_url = f"{SITE_URL}{download.attrs['href']}"
-        r = session.get(download_url)
+        r = session.get(download_url, proxies=PROXIES)
 
         pans = r.html.find('.text-danger')
         pan = pans[1] if len(pans) > 1 else pans[0]
@@ -56,19 +65,7 @@ class Spider:
         pan_key = r.html.search('提取码:{}</p>')[0]
         return pan_url, pan_key
 
-    def get_all_bools(self):
-        for i in range(1, 46):
-            url = f'{SITE_URL}/book/?sort_by=created_at&page={i}'
-            r = session.get(url)
-            books_list = r.html.find('.list-unstyled', first=True)
-            books = books_list.find('.media .mt-0 a')
-            for book in books:
-                bool_title, book_link = book.text, f"{SITE_URL}{book.attrs['href']}"
-                pan_url, pan_key = self.get_book(book_link)
-                print(f'{bool_title}, {pan_url}, {pan_key}')
-
 
 if __name__ == '__main__':
     s = Spider()
-    # s.get_books()
-    s.get_all()
+    s.get_books()
